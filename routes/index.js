b2380df6e22ad2d57dcc73333bf5ec785a1ba42f
Viet Vo <viet.allen.qvo@gmail.com>
2018-10-15 03:53:01 -0700

    Can scrape information but incorrectly formatted

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,27 @@
 // import db from "../models";
 const express = require("express");
 const app = express();
-const axoios = require("axios");
 const cheerio = require("cheerio");
 const link = "https://www.amazon.com/s/ref=nb_sb_ss_i_1_12?url=search-alias%3Daps&field-keywords=mens+jean+shorts&sprefix=mens+jean+sh%2Caps%2C219&crid=3NVWVJSYHNA2M";
+const request = require("request");
 
-app.get("/scrape", (req, res) => {
-    axios.get(link).then(response => {
-        const $ = cheerio.load(response.data);
-        let results = [];
-        $("img.s-access-image").each((i, element) => {
-            console.log(element);
-            const image = $(element).attr("src");
-            results.push({
-                image: image
-            });
+// app.get("/scrape", (req, res) => {
+request(link, (error, response, html) => {
+    const $ = cheerio.load(html);
+    let results = [];
+    $("img.s-access-image").each((i, element) => {
+        const image = $(element).attr("src");
+        results.push({
+            image: image
         });
-        console.log(results);
     });
-
+    $("h2.a-size-small").each((i, element) => {
+        const title = $(element).text();
+        results.push({
+            title: title
+        })
+    });
+    console.log(results);
 });
+// });
 // axios.get(link).each()
